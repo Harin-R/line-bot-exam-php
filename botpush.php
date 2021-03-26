cba785b6c0321a4d48773d83d739a9f3364c263e
Harin-R <81366739+Harin-R@users.noreply.github.com>
2021-03-26 14:21:24 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -22,10 +22,13 @@ $response_A = $bot->pushMessage($pushID_A, $textMessageBuilder);
 $response_Mew = $bot->pushMessage($pushID_Mew, $textMessageBuilder);
 $response_Mint = $bot->pushMessage($pushID_Mint, $textMessageBuilder);
 
-echo $response_Peem->getHTTPStatus() . ' ' . $response_Peem->getRawBody()<br />;<br/>
-echo $response_A->getHTTPStatus() . ' ' . $response_A->getRawBody()<br />;<br/>
-echo $response_Mew->getHTTPStatus() . ' ' . $response_Mew->getRawBody()<br />;<br/>
-echo $response_Mint->getHTTPStatus() . ' ' . $response_Mint->getRawBody()<br />;<br/>
+echo $response_Peem->getHTTPStatus() . ' ' . $response_Peem->getRawBody();
+echo <br/>
+echo $response_A->getHTTPStatus() . ' ' . $response_A->getRawBody();
+echo <br/>
+echo $response_Mew->getHTTPStatus() . ' ' . $response_Mew->getRawBody();
+echo <br/>
+echo $response_Mint->getHTTPStatus() . ' ' . $response_Mint->getRawBody();
 
 
 
